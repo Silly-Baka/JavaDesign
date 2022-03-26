0280dad9e7d6f6f450a37d75627e86667080a5f0
silly_baka <1017165985@qq.com>
2022-03-26 23:41:02 +0800

    master commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("master test");
     }
 }
