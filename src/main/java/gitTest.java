1177011f6e601ba09edf77e386bf7bf7c233f1c5
silly_baka <1017165985@qq.com>
2022-03-26 23:36:40 +0800

    hot-fix first commit

---
+++ src/main/java/gitTest.java
@@ -0,0 +1,11 @@
+/**
+ * User: 86176
+ * Date: 2022/3/26
+ * Time: 23:34
+ * Description:
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
